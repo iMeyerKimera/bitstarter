8df51849849316f42e72001da3d99526ee215b75
Meyer Kimera <aremik@live.com>
2013-07-17 15:22:57 +0000

    Update  web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+
+
 var express = require('express');
+var fs = require('fs');
+var html = "index.html";
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('bitstarter/index.html')); 
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html); 
 });
 
 var port = process.env.PORT || 5000;
